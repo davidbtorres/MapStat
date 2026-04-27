28159ac7a83d077b6a24584a0b5d5c57a9317483
David Torres <dtorres@ecg-hq.com>
2026-04-27 09:01:59 -0600

    first commit

---
+++ src/components/MapView.tsx
@@ -0,0 +1,16 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+
+export default function MapView() {
+  return (
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution="&copy; OpenStreetMap contributors"
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
